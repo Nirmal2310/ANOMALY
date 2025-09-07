560d6331bd805e0143598cbaacc2c68d12f29c91
Nirmal <62856422+Nirmal2310@users.noreply.github.com>
2025-09-07 23:10:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,111 +32,21 @@ data_dir=$(echo "$script_dir" | sed 's/$/\/DATA/g')
 
 ## Installing Required Tools for the Pipeline
 
-if { conda env list |  grep -w "snakemake"; } > /dev/null 2>&1; then
+if { conda env list |  grep -w "anomaly"; } > /dev/null 2>&1; then
 
-        conda list -n snakemake --explicit > _current_env.txt
+        conda list -n anomaly --explicit > _current_env.txt
 
-        if diff -q $script_dir/Envs/snakemake.txt _current_env.txt > /dev/null; then
+        if diff -q $script_dir/Envs/anomaly.txt _current_env.txt > /dev/null; then
                 echo "Environment exists and up to date." && rm -r _current_env.txt
         else
-                conda create --name snakemake --file $script_dir/Envs/snakemake.txt -y && rm -r _current_env.txt
-                conda list -n snakemake --explicit > $script_dir/Envs/snakemake.txt
+                conda create --name anomaly --file $script_dir/Envs/anomaly.txt -y && rm -r _current_env.txt
+                conda list -n anomaly --explicit > $script_dir/Envs/anomaly.txt
         fi
 
 else
 
-        conda create --name snakemake --file $script_dir/Envs/snakemake.txt
-        conda list -n snakemake --explicit > $script_dir/Envs/snakemake.txt
-
-fi
-
-if { conda env list |  grep -w "minimap2"; } > /dev/null 2>&1; then
-
-        conda list -n minimap2 --explicit > _current_env.txt
-
-        if diff -q $script_dir/Envs/minimap2.txt _current_env.txt > /dev/null; then
-                echo "Environment exists and up to date." && rm -r _current_env.txt
-        else
-                conda create --name minimap2 --file $script_dir/Envs/minimap2.txt -y && rm -r _current_env.txt
-                conda list -n minimap2 --explicit > $script_dir/Envs/minimap2.txt
-        fi
-
-else
-
-        conda create --name minimap2 --file $script_dir/Envs/minimap2.txt
-        conda list -n minimap2 --explicit > $script_dir/Envs/minimap2.txt
-
-fi
-
-if { conda env list |  grep -w "sniffles"; } > /dev/null 2>&1; then
-
-        conda list -n sniffles --explicit > _current_env.txt
-
-        if diff -q $script_dir/Envs/sniffles.txt _current_env.txt > /dev/null; then
-                echo "Environment exists and up to date." && rm -r _current_env.txt
-        else
-                conda create --name sniffles --file $script_dir/Envs/sniffles.txt -y && rm -r _current_env.txt
-                conda list -n sniffles --explicit > $script_dir/Envs/sniffles.txt
-        fi
-
-else
-
-        conda create --name sniffles --file $script_dir/Envs/sniffles.txt
-        conda list -n sniffles --explicit > $script_dir/Envs/sniffles.txt
-
-fi
-
-if { conda env list |  grep -w "blast"; } > /dev/null 2>&1; then
-
-        conda list -n blast --explicit > _current_env.txt
-
-        if diff -q $script_dir/Envs/blast.txt _current_env.txt > /dev/null; then
-                echo "Environment exists and up to date." && rm -r _current_env.txt
-        else
-                conda create --name blast --file $script_dir/Envs/blast.txt -y && rm -r _current_env.txt
-                conda list -n blast --explicit > $script_dir/Envs/blast.txt
-        fi
-
-else
-
-        conda create --name blast --file $script_dir/Envs/blast.txt
-        conda list -n blast --explicit > $script_dir/Envs/blast.txt
-
-fi
-
-if { conda env list |  grep -w "bedtools"; } > /dev/null 2>&1; then
-
-        conda list -n bedtools --explicit > _current_env.txt
-
-        if diff -q $script_dir/Envs/bedtools.txt _current_env.txt > /dev/null; then
-                echo "Environment exists and up to date." && rm -r _current_env.txt
-        else
-                conda create --name bedtools --file $script_dir/Envs/bedtools.txt -y && rm -r _current_env.txt
-                conda list -n bedtools --explicit > $script_dir/Envs/bedtools.txt
-        fi
-
-else
-
-        conda create --name bedtools --file $script_dir/Envs/bedtools.txt
-        conda list -n bedtools --explicit > $script_dir/Envs/bedtools.txt
-
-fi
-
-if { conda env list |  grep -w "renv"; } > /dev/null 2>&1; then
-
-        conda list -n renv --explicit > _current_env.txt
-
-        if diff -q $script_dir/Envs/renv.txt _current_env.txt > /dev/null; then
-                echo "Environment exists and up to date." && rm -r _current_env.txt
-        else
-                conda create --name renv --file $script_dir/Envs/renv.txt -y && rm -r _current_env.txt
-                conda list -n renv --explicit > $script_dir/Envs/renv.txt
-        fi
-
-else
-
-        conda create --name renv --file $script_dir/Envs/renv.txt
-        conda list -n renv --explicit > $script_dir/Envs/renv.txt
+        conda create --name anomaly --file $script_dir/Envs/anomaly.txt
+        conda list -n anomaly --explicit > $script_dir/Envs/anomaly.txt
 
 fi
 
@@ -146,15 +56,13 @@ if [ ! -d "$data_dir" ]; then
 
  	mkdir "$data_dir"
 
-	wget https://ftp.ensembl.org/pub/release-113/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz -O "$data_dir"/Homo_sapiens.GRCh38.MT.fasta.gz
-
-	gunzip "$data_dir/"Homo_sapiens.GRCh38.MT.fasta.gz
+	wget https://www.ebi.ac.uk/ena/browser/api/fasta/CP068254.1 -O "$data_dir"/Homo_sapiens.CHM13.MT.fasta
 
  	source $path/bin/activate blast
 
-  	seqkit concat -w 0 -t DNA "$data_dir"/Homo_sapiens.GRCh38.MT.fasta "$data_dir"/Homo_sapiens.GRCh38.MT.fasta > "$data_dir"/Homo_sapiens.GRCh38.MT_concatenated.fasta
+  	seqkit concat -w 0 -t DNA "$data_dir"/Homo_sapiens.CHM13.MT.fasta "$data_dir"/Homo_sapiens.CHM13.MT.fasta > "$data_dir"/Homo_sapiens.CHM13.MT_concatenated.fasta
 
-	makeblastdb -in "$data_dir"/Homo_sapiens.GRCh38.MT_concatenated.fasta -parse_seqids -blastdb_version 5 -title Homo_sapiens.GRCh38.MT_concatenated.fasta -dbtype nucl -out "$data_dir"/Homo_sapiens.GRCh38.MT_concatenated.fasta
+	makeblastdb -in "$data_dir"/Homo_sapiens.CHM13.MT_concatenated.fasta -parse_seqids -blastdb_version 5 -title Homo_sapiens.CHM13.MT_concatenated.fasta -dbtype nucl -out "$data_dir"/Homo_sapiens.CHM13.MT_concatenated.fasta
 
 	source ~/.bashrc
 
@@ -164,4 +72,4 @@ if [ ! -d "$data_dir" ]; then
 	
 fi
 
-grep -qF "export MT_DATA=\"$data_dir/Homo_sapiens.GRCh38.MT_concatenated.fasta\"" ~/.bashrc || echo "export MT_DATA=\"$data_dir/Homo_sapiens.GRCh38.MT_concatenated.fasta\"" >> ~/.bashrc && source ~/.bashrc
+grep -qF "export MT_DATA=\"$data_dir/Homo_sapiens.CHM13.MT_concatenated.fasta\"" ~/.bashrc || echo "export MT_DATA=\"$data_dir/Homo_sapiens.CHM13.MT_concatenated.fasta\"" >> ~/.bashrc && source ~/.bashrc
