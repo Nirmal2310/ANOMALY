74c9d25530e3fe4cc7aa4407bd684c7d8dee55b7
Nirmal <62856422+Nirmal2310@users.noreply.github.com>
2025-09-30 15:24:14 +0530

    improved numt concatenation script

--- Scripts/numt_concat.sh
+++ Scripts/numt_concat.sh
@@ -17,6 +17,10 @@ output="$2"
 
 tmp_file=$(mktemp)
 
+tmp_file_new=$(mktemp)
+    
+concat_numt_loc=$(mktemp)
+
 if [ -f "$MT_DATA" ]; then
     mt_length=$(cat "$MT_DATA" | awk '/^>/{if (l!="") print; l=0; next}{l+=length($0)}END{print l/2}')
 else
@@ -35,6 +39,8 @@ process_freq_6() {
     local start="$3"
     local end="$4"
     local mt_length="$5"
+    tmp_file_2=$(mktemp)
+    passed_calls=$(mktemp)
 
     # Filter by chromosome and position
     awk -v chr="$chr" -v start="$start" -v end="$end" '
@@ -50,22 +56,22 @@ process_freq_6() {
         } else {
             print $0
         }
-    }' | sort -k8 -n | uniq | \
-    awk 'BEGIN{FS=OFS="\t"} {
-        lines[NR] = $0
-        if($2 < chr_min || NR==1) chr_min = $2
-        if($3 < chr_max || NR==1) chr_max = $3
-        if($5 < mt_min || NR==1) mt_min = $5
-        if($6 > mt_max || NR==1) mt_max = $6
-        if($7 > query_max || NR==1) query_max = $7
-        if($8 > ref_max || NR==1) ref_max = $8
-    } END{
-        if(NR==3) {
-            print $1,chr_min,chr_max,$4,mt_min,mt_max,query_max,ref_max
-        } else if(NR==6) {
-            print lines[NR]
-        }
-    }'
+    }' | sort -k8 -n | uniq > "$tmp_file_2"
+
+    awk -F "\t" '{if(($8*100/$7)>=70) print $0}' "$tmp_file_2" > "$passed_calls"
+
+    if [ -s "$passed_calls" ]; then
+       lines=$(cat "$passed_calls" | wc -l)
+        if [ "$lines" -gt 1 ]; then
+            sort -k8 -n -r "$passed_calls" | head -n 1
+            rm -r "$passed_calls"
+        else
+            cat "$passed_calls"
+            rm -r "$passed_calls"
+        fi
+    else 
+        process_concatenated_numts "$tmp_file_2" "$chr" "$start" "$end" "$mt_length"
+    fi
 }
 
 process_freq_4() {
@@ -74,6 +80,8 @@ process_freq_4() {
     local start="$3"
     local end="$4"
     local mt_length="$5"
+    tmp_file_2=$(mktemp)
+    passed_calls=$(mktemp)
 
     # Filter by chromosome and position
     awk -v chr="$chr" -v start="$start" -v end="$end" '
@@ -90,23 +98,68 @@ process_freq_4() {
         } else {
             print $0
         }
-    }' | sort -k8 -n | uniq | \
-    # Calculate min/max values
-    awk 'BEGIN {FS=OFS="\t"} {
-        lines[NR] = $0
-        if($2 < chr_min || NR==1) chr_min = $2
-        if($3 < chr_max || NR==1) chr_max = $3
-        if($5 < mt_min || NR==1) mt_min = $5
-        if($6 > mt_max || NR==1) mt_max = $6
-        if($7 > query_max || NR==1) query_max = $7
-    }
-    END {
-        if(NR==2) {
-            print $1,chr_min,chr_max,$4,mt_min,mt_max,query_max,mt_max-mt_min
-        } else if(NR==4) {
-            print lines[NR]
-        }
-    }'
+    }' | sort -k8 -n | uniq > "$tmp_file_2"
+
+    awk -F "\t" '{if(($8*100/$7)>=70) print $0}' "$tmp_file_2" > "$passed_calls"
+
+    if [ -s "$passed_calls" ]; then
+        lines=$(cat "$passed_calls" | wc -l)
+        if [ "$lines" -gt 1 ]; then
+            sort -k8 -n -r "$passed_calls" | head -n 1
+            rm -r "$passed_calls"
+        else
+            cat "$passed_calls"
+            rm -r "$passed_calls"
+        fi
+    else
+        process_concatenated_numts "$tmp_file_2" "$chr" "$start" "$end" "$mt_length"
+    fi
+}
+
+process_concatenated_numts() {
+    local in="$1"
+    local chr="$2"
+    local start="$3"
+    local end="$4"
+    local mt_length="$5"
+    mt_tmp_file=$(mktemp)
+
+    awk 'BEGIN{FS="\t";OFS="\t"}{print $4, $5, $6}' "$in" | sortBed | bedtools merge -i stdin -d 100 > "$mt_tmp_file"
+
+    cat "$mt_tmp_file"
+
+    line_count=$(cat "$mt_tmp_file" | wc -l)
+
+    if [ "$line_count" -eq 1 ]; then 
+        mt_start=$(awk -F "\t" '{print $2}' "$mt_tmp_file")
+        mt_end=$(awk -F "\t" '{print $3}' "$mt_tmp_file")
+        awk -v mt_start="$mt_start" -v mt_end="$mt_end" '
+        BEGIN {FS=OFS="\t"}
+        {
+            lines[NR] = $0
+            if($2 < chr_min || NR==1) chr_min = $2
+            if($3 < chr_max || NR==1) chr_max = $3
+            if($7 > query_max || NR==1) query_max = $7
+            ref_sum += $8
+        } END {
+            print $1, chr_min, chr_max,$4, mt_start, mt_end, query_max, ref_sum
+        }' "$in"
+    else 
+        while IFS=$'\t' read -r mt mt_start mt_end; do
+            awk -v mt_start="$mt_start" -v mt_end="$mt_end" '
+            BEGIN {FS=OFS="\t"}
+            {
+                lines[NR] = $0
+                if($2 < chr_min || NR==1) chr_min = $2
+                if($3 < chr_max || NR==1) chr_max = $3
+                if($7 > query_max || NR==1) query_max = $7
+                ref_sum +=$8
+            } END {
+                print $1, chr_min, chr_max, $4, mt_start, mt_end, query_max, ref_sum
+            }' "$in"
+        done < "$mt_tmp_file"
+    
+    fi
 }
 
 process_freq_3() {
@@ -122,7 +175,7 @@ process_freq_3() {
     }' "$in" | sort -k8 -n -r | head -n 1
 }
 
-process_other_freq() {
+process_freq_2() {
     local in="$1"
     local chr="$2"
     local start="$3"
@@ -159,6 +212,46 @@ process_other_freq() {
     }'
 }
 
+process_other_freq() {
+    local in="$1"
+    local chr="$2"
+    local start="$3"
+    local end="$4"
+    local mt_length="$5"
+    tmp_file_2=$(mktemp)
+    passed_calls=$(mktemp)
+
+    awk -v chr="$chr" -v start="$start" -v end="$end" '
+    BEGIN {FS=OFS="\t"}
+    $1==chr && $2>=start && $3<=end {
+        print $0
+    }' "$in" | \
+    awk -v mt_len="$mt_length" '
+    BEGIN {FS=OFS="\t"} {
+        if($5>mt_len && $6>mt_len) {
+            print $1,$2,$3,$4,$5-mt_len,$6-mt_len,$7,$8
+        } else {
+            print $0
+        }
+    }' | sort -k8 -n | uniq > "$tmp_file_2"
+
+    awk -F "\t" '{if(($8*100/$7)>=70) print $0}' "$tmp_file_2" > "$passed_calls"
+
+    if [ -s "$passed_calls" ]; then
+        lines=$(cat "$passed_calls" | wc -l)
+        if [ "$lines" -gt 1 ]; then
+            sort -k8 -n -r "$passed_calls" | head -n 1
+            rm -r "$passed_calls"
+        else
+            cat "$passed_calls"
+            rm -r "$passed_calls"
+        fi
+    else 
+        process_concatenated_numts "$tmp_file_2" "$chr" "$start" "$end" "$mt_length"
+    fi
+}
+
+
 concat_numt() {
     while IFS=$'\t' read -r chr start end freq; do
         case "$freq" in
@@ -173,7 +266,12 @@ concat_numt() {
                 process_freq_3 "$1" "$chr" "$start" "$end" "$3"
                 ;;
             2)
-                process_other_freq "$1" "$chr" "$start" "$end" "$3"
+                process_freq_2 "$1" "$chr" "$start" "$end" "$3"
+                ;;
+            *)
+                if [ "$freq" -gt 3 ] && [ "$freq" -ne 4 ] && [ "$freq" -ne 6 ]; then
+                    process_other_freq "$1" "$chr" "$start" "$end" "$3"
+                fi
                 ;;
         esac
     done < "$2"
@@ -184,21 +282,40 @@ if [ -s "$input" ]; then
     
     get_freq "$input" > "$tmp_file"
     
-    concat_numt "$input" "$tmp_file" "$mt_length" | awk 'BEGIN{FS="\t";OFS="\t"} {
+    concat_numt "$input" "$tmp_file" "$mt_length" \
+    | awk 'BEGIN{FS="\t";OFS="\t"} {
         if(($8*100)/$7 >= 70) print $1,$2,$3,$4,$5,$6
     }' | awk -v mt_len="$mt_length" 'BEGIN{FS=OFS="\t"} {
         if($6>mt_len) {
             print $1, $2, $3, $4, $5, $6-mt_len, "With Control"
         }
-            else { print $0, "Without Control"
-            }
+        else {
+            print $0, "Without Control"
+        }
     }' | awk 'BEGIN{FS=OFS="\t"} {
     	if($5>$6) {
     	   print $1, $2, $3, $4, $6, $5, $7
-    	} else { print $1, $2, $3, $4, $5, $6, $7}
-    	}' > "$output"
+    	} else {
+            print $1, $2, $3, $4, $5, $6, $7}
+    	}' > "$tmp_file_new"
 
-    rm -r "$tmp_file"
+    cat "$tmp_file_new" | awk -F "\t" '{print $1"\t"$2"\t"$3}' | sort | uniq -cd | awk '{print $2"\t"$3"\t"$4}' > "$concat_numt_loc"
+
+    if [ -s "$concat_numt_loc" ]; then
+        while IFS=$'\t' read -r chr start end; do
+            awk -v chr="$chr" -v start="$start" -v end="$end" 'BEGIN{FS=OFS="\t"} {
+                if ($1==chr && $2==start && $3==end) {
+                    print $1, $2, $3, $4, $5, $6, "Concatenated NUMT"
+                } else {
+                    print $0
+                }
+            }' "$tmp_file_new"
+        done < "$concat_numt_loc" > "$output"
+    else
+        cat "$tmp_file_new" > "$output"
+    fi
+    
+    rm -r "$tmp_file" "$tmp_file_new" "$concat_numt_loc"
 
 else
     echo "Input file is empty."
